945f389f1dcd78155b2014bdbfaa16bff4f9cd7d
irconde <ivanrodriguezconde@gmail.com>
2019-11-05 22:57:54 -0600

    Basic asyntask declaration and execution

---
+++ app/src/main/java/edu/ualr/asynchronousprogramming/DownloadImageTask.java
@@ -0,0 +1,104 @@
+package edu.ualr.asynchronousprogramming;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.widget.ImageView;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.ref.WeakReference;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by irconde on 2019-11-05.
+ */
+// TODO 01. Define a new class that extends the AsyncTask class
+// We have to specify the three type parameters that exposes the AsyncTask class
+    // Params. Type of the value we pass to doInBackground. URL
+    // Progress. Type of the value returned to the main thread while the background thread is running. Integer
+    // Result. Type of the value returned by the AsyncTask. Bitmap
+public class DownloadImageTask extends AsyncTask<URL, Integer, Bitmap> {
+
+    // TODO 02. Weak reference to the UI View to update
+    // The WeakReference does not prevent the view from being garbage collected when the activity
+    // where the view was created is no longer active.
+    private final WeakReference<ImageView> imageViewRef;
+
+    // TODO 03. Constructor. Initialize the weak reference to the UI View
+    public DownloadImageTask(ImageView imageView) {
+        this.imageViewRef = new WeakReference<>(imageView);
+    }
+
+    // Retrieves the image from a URL
+    private Bitmap downloadBitmap(URL url) {
+        Bitmap bitmap =null;
+        InputStream is = null;
+        try {
+            if (isCancelled()) {
+                return null;
+            }
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            int responseCode = conn.getResponseCode();
+
+            if (responseCode != HttpURLConnection.HTTP_OK){
+                throw new Exception("Unsucesfull Result code");
+            }
+            is = conn.getInputStream();
+            BufferedInputStream bif = new BufferedInputStream(is) {
+                public int read(byte[] buffer, int byteOffset, int byteCount) throws IOException {
+                    int readBytes = super.read(buffer, byteOffset, byteCount);
+                    if ( isCancelled() ){
+                        // Returning -1 means that there is no more data because the
+                        // end of the stream has been reached.
+                        return -1;
+                    }
+                    return readBytes;
+                }
+            };
+            Bitmap downloaded = BitmapFactory.decodeStream(bif);
+            if ( !isCancelled() ){
+                bitmap = downloaded;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return bitmap;
+    }
+
+    // TODO 04. Override doInBackground method to define the operations we want to
+    //  execute in a background thread. Download an image from the provided URL
+    @Override
+    protected Bitmap doInBackground(URL... urls) {
+        URL url = urls[0];
+        // The IO operation invoked will take a significant ammount
+        // to complete
+        return downloadBitmap(url);
+    }
+
+    // TODO 05. Override onPostExecute method to return the result of the operation executed by the
+    //  background thread to the main thread
+    @Override
+    protected void onPostExecute(Bitmap bitmap) {
+        ImageView imageView = this.imageViewRef.get();
+        if (imageView != null) {
+            imageView.setImageBitmap(bitmap);
+        }
+    }
+}
--- app/src/main/java/edu/ualr/asynchronousprogramming/MainActivity.java
+++ app/src/main/java/edu/ualr/asynchronousprogramming/MainActivity.java
@@ -2,13 +2,48 @@ package edu.ualr.asynchronousprogramming;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
+    // TODO 06. Define am AsyncTask member
+    AsyncTask photoAsyncTask = null;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button showBut = findViewById(R.id.showImageBut);
+        showBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    URL url = new URL("http://media.mydogspace.com.s3.amazonaws.com/wp-content" +
+                            "/uploads/2013/08/puppy-500x350.jpg");
+                    // TODO 07. We create a new instance of the AsyncTask, providing a reference to the
+                    // ImageView element used to display the downloaded image
+                    // TODO 08. We execute the AsyncTask providing as input parameter the URL of the image
+                    ImageView iv = findViewById(
+                            R.id.downloadedImage);
+                    photoAsyncTask = new DownloadImageTask(iv)
+                            .execute(url);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
