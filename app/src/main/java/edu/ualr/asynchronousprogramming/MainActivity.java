70693ad974ea6beecc4a4179f6be2b4702a832bf
irconde <ivanrodriguezconde@gmail.com>
2019-11-06 09:33:33 -0600

    Execution states

--- app/src/main/java/edu/ualr/asynchronousprogramming/MainActivity.java
+++ app/src/main/java/edu/ualr/asynchronousprogramming/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -13,6 +14,7 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
     AsyncTask photoAsyncTask = null;
 
     @Override
@@ -27,10 +29,10 @@ public class MainActivity extends AppCompatActivity {
                     URL url = new URL("http://media.mydogspace.com.s3.amazonaws.com/wp-content" +
                             "/uploads/2013/08/puppy-500x350.jpg");
                     // ImageView element used to display the downloaded image
-                    ImageView iv = findViewById(
-                            R.id.downloadedImage);
-                    photoAsyncTask = new DownloadImageTask(MainActivity.this, iv)
-                            .execute(url);
+                    ImageView iv = findViewById(R.id.downloadedImage);
+                    photoAsyncTask = new DownloadImageTask(MainActivity.this, iv).execute(url);
+                    // TODO 01. Add a new method so we can get the status of the background task using the getStatus method
+                    logAsyncTaskStatus(photoAsyncTask);
                 } catch (MalformedURLException e) {
                     e.printStackTrace();
                 }
@@ -39,8 +41,32 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    // TODO 01. Method definition
+    private void logAsyncTaskStatus(AsyncTask task) {
+        switch (task.getStatus()) {
+            case PENDING:
+                Log.d(TAG, "Task has not started yet. We can invoke execute");
+                break;
+            case RUNNING:
+                Log.d(TAG, "Task currently running in background");
+                break;
+            case FINISHED:
+                if (task.isCancelled()) {
+                    Log.d(TAG, "Task is done. OnCancelled was called");
+                } else {
+                    Log.d(TAG, "Task is done. OnPostExecute was called");
+                }
+                break;
+                default: Log.d(TAG , "Unknown state");
+        }
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        // TODO 02. Cancel Pending Tasks when the activity is destroyed
+        if(photoAsyncTask !=null && photoAsyncTask.getStatus()!=AsyncTask.Status.FINISHED){
+            photoAsyncTask.cancel(true);
+        }
     }
 }
