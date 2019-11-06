eef3906e47d7ff91bfc10d6ef64c7021ae603192
irconde <ivanrodriguezconde@gmail.com>
2019-11-05 21:44:22 -0600

    Starter code

---
+++ app/src/main/java/edu/ualr/asynchronousprogramming/DownloadImageTask.java
@@ -0,0 +1,106 @@
+package edu.ualr.asynchronousprogramming;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.widget.ImageView;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.ref.WeakReference;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by irconde on 2019-11-05.
+ */
+// We have to specify the three type parameters that exposes the AsyncTask class
+    // Params. Type of the value we pass to doInBackground. URL
+    // Progress. Type of the value returned to the main thread while the background thread is running. Integer
+    // Result. Type of the value returned by the AsyncTask. Bitmap
+public class DownloadImageTask extends AsyncTask<URL, Integer, Bitmap> {
+
+    // The WeakReference does not prevent the view from being garbage collected when the activity
+    // where the view was created is no longer active.
+    private final WeakReference<ImageView> imageViewRef;
+
+    // TODO 01. We add a weak reference to the context
+
+    // TODO 03. We define a new ProgressDialog member. It's used to provide the user with information
+    //  about the progress of the background
+
+    // TODO 02. We initialize the context reference in the constructor
+    public DownloadImageTask(ImageView imageView) {
+        this.imageViewRef = new WeakReference<>(imageView);
+    }
+
+    // TODO 04. We override the onPreExecute method to define the actions to be executed before the
+    //  background thread starts
+    @Override
+    protected void onPreExecute() {
+            // TODO 05. We create a new ProgressDialog instance
+            // TODO 06. We initialize the progress dialog.
+            // TODO 06.01. Make the progress dialog noncancelable and indeterminate
+            // TODO 07. Show the progress dialog
+    }
+
+    // Retrieves the image from a URL
+    private Bitmap downloadBitmap(URL url) {
+        Bitmap bitmap =null;
+        InputStream is = null;
+        try {
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            int responseCode = conn.getResponseCode();
+
+            if (responseCode != HttpURLConnection.HTTP_OK){
+                throw new Exception("Unsucesfull Result code");
+            }
+            is = conn.getInputStream();
+            BufferedInputStream bif = new BufferedInputStream(is) {
+                public int read(byte[] buffer, int byteOffset, int byteCount) throws IOException {
+                    int readBytes = super.read(buffer, byteOffset, byteCount);
+                    return readBytes;
+                }
+            };
+            Bitmap downloaded = BitmapFactory.decodeStream(bif);
+            bitmap = downloaded;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return bitmap;
+    }
+
+    @Override
+    protected Bitmap doInBackground(URL... urls) {
+        URL url = urls[0];
+        // The IO operation invoked will take a significant ammount
+        // to complete
+        return downloadBitmap(url);
+    }
+
+    @Override
+    protected void onPostExecute(Bitmap bitmap) {
+        // TODO 08. Hide the progress dialog once the background work is finished
+        ImageView imageView = this.imageViewRef.get();
+        if (imageView != null) {
+            imageView.setImageBitmap(bitmap);
+        }
+    }
+}
---
+++ app/src/main/java/edu/ualr/asynchronousprogramming/MainActivity.java
@@ -0,0 +1,47 @@
+package edu.ualr.asynchronousprogramming;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    AsyncTask photoAsyncTask = null;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button showBut = findViewById(R.id.showImageBut);
+        showBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    URL url = new URL("http://media.mydogspace.com.s3.amazonaws.com/wp-content" +
+                            "/uploads/2013/08/puppy-500x350.jpg");
+                    // ImageView element used to display the downloaded image
+                    ImageView iv = findViewById(
+                            R.id.downloadedImage);
+                    // TODO 09. Pass the context to the constructor of the AsyncTask
+                    photoAsyncTask = new DownloadImageTask(iv)
+                            .execute(url);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
